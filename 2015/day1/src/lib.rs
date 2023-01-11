04c09feb31e56fbd9b537929c7344ade4f93158d
Ostap Sulyk <ostapsulyk@gmail.com>
2023-01-11 08:49:21 -0500

    Task 2 ✅

--- 2015/day1/src/lib.rs
+++ 2015/day1/src/lib.rs
@@ -8,3 +8,24 @@ pub fn task1() -> i32 {
         })
         .sum()
 }
+
+pub fn task2() -> i32 {
+    let data: Vec<i32> = include_str!("input.txt")
+        .split("")
+        .map(|c| match c {
+            ")" => -1,
+            "(" => 1,
+            _ => 0,
+        })
+        .collect();
+    let mut floor = 0;
+    let mut basement_index = 0;
+    for instruction in data {
+        floor += instruction;
+        if floor == -1 {
+            break;
+        }
+        basement_index += 1
+    }
+    basement_index
+}
