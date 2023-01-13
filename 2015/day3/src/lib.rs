a2fa154b3107859dad7841e5b137e261f4279c82
Ostap Sulyk <ostapsulyk@gmail.com>
2023-01-12 22:58:56 -0500

    Day 3 task 2 ✅

--- 2015/day3/src/lib.rs
+++ 2015/day3/src/lib.rs
@@ -1,16 +1,46 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 pub fn task1() -> i32 {
-    let test1: Vec<char> = ">".chars().collect();
-    let test2: Vec<char> = "^>v<".chars().collect();
-    let test3: Vec<char> = "^v^v^v^v^v".chars().collect();
-
     let data: Vec<char> = include_str!("input.txt").chars().collect();
 
-    calc_houses(&data)
+    calc_houses1(&data)
+}
+pub fn task2() -> i32 {
+    let data: Vec<char> = include_str!("input.txt").chars().collect();
+    calc_houses2(&data)
 }
 
-pub fn calc_houses(instructions: &Vec<char>) -> i32 {
+fn calc_houses2(instructions: &Vec<char>) -> i32 {
+    let (mut santa_x, mut santa_y) = (0, 0);
+    let (mut robo_x, mut robo_y) = (0, 0);
+    let mut my_set: HashSet<(i32, i32)> = HashSet::new();
+
+    my_set.insert((santa_x, santa_y));
+
+    for (i, c) in instructions.iter().enumerate() {
+        if i % 2 == 0 {
+            match c {
+                '>' => santa_x += 1,
+                '<' => santa_x -= 1,
+                '^' => santa_y += 1,
+                _ => santa_y -= 1,
+            }
+        } else {
+            match c {
+                '>' => robo_x += 1,
+                '<' => robo_x -= 1,
+                '^' => robo_y += 1,
+                _ => robo_y -= 1,
+            }
+        }
+        my_set.insert((robo_x, robo_y));
+        my_set.insert((santa_x, santa_y));
+    }
+
+    my_set.len() as i32
+}
+
+fn calc_houses1(instructions: &Vec<char>) -> i32 {
     let mut x = 0;
     let mut y = 0;
     let mut my_set: HashSet<(i32, i32)> = HashSet::new();
--- 2015/day3/src/main.rs
+++ 2015/day3/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    println!("Day 3 Task 1: {}", day3::task1());
-    println!("Day 3 Task 2: {}", day3::task1());
+    println!("Day 3 Task 1: {}",day3::task1());
+    println!("Day 3 Task 2: {}",day3::task2());
 }
