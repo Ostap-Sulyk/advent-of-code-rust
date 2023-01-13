9086479accfcff87ba94f3be77553fb137a8048c
Ostap Sulyk <ostapsulyk@gmail.com>
2023-01-12 23:10:10 -0500

    Refactored code for better readability

--- 2015/day3/src/lib.rs
+++ 2015/day3/src/lib.rs
@@ -10,6 +10,19 @@ pub fn task2() -> i32 {
     calc_houses2(&data)
 }
 
+fn calc_houses1(instructions: &Vec<char>) -> i32 {
+    let (mut x, mut y) = (0, 0);
+    let mut my_set: HashSet<(i32, i32)> = HashSet::new();
+    my_set.insert((x, y));
+
+    for i in instructions {
+        matcher(i, (&mut x, &mut y));
+        my_set.insert((x, y));
+    }
+
+    my_set.len() as i32
+}
+
 fn calc_houses2(instructions: &Vec<char>) -> i32 {
     let (mut santa_x, mut santa_y) = (0, 0);
     let (mut robo_x, mut robo_y) = (0, 0);
@@ -18,21 +31,11 @@ fn calc_houses2(instructions: &Vec<char>) -> i32 {
     my_set.insert((santa_x, santa_y));
 
     for (i, c) in instructions.iter().enumerate() {
-        if i % 2 == 0 {
-            match c {
-                '>' => santa_x += 1,
-                '<' => santa_x -= 1,
-                '^' => santa_y += 1,
-                _ => santa_y -= 1,
-            }
-        } else {
-            match c {
-                '>' => robo_x += 1,
-                '<' => robo_x -= 1,
-                '^' => robo_y += 1,
-                _ => robo_y -= 1,
-            }
+        match i % 2 {
+            0 => matcher(c, (&mut santa_x, &mut santa_y)),
+            _ => matcher(c, (&mut robo_x, &mut robo_y)),
         }
+
         my_set.insert((robo_x, robo_y));
         my_set.insert((santa_x, santa_y));
     }
@@ -40,21 +43,11 @@ fn calc_houses2(instructions: &Vec<char>) -> i32 {
     my_set.len() as i32
 }
 
-fn calc_houses1(instructions: &Vec<char>) -> i32 {
-    let mut x = 0;
-    let mut y = 0;
-    let mut my_set: HashSet<(i32, i32)> = HashSet::new();
-
-    my_set.insert((x, y));
-    for i in instructions {
-        match i {
-            '>' => x += 1,
-            '<' => x -= 1,
-            '^' => y += 1,
-            _ => y -= 1,
-        }
-        my_set.insert((x, y));
+fn matcher(character: &char, coords: (&mut i32, &mut i32)) {
+    match character {
+        '>' => *coords.0 += 1,
+        '<' => *coords.0 -= 1,
+        '^' => *coords.1 += 1,
+        _ => *coords.1 -= 1,
     }
-
-    my_set.len() as i32
 }
